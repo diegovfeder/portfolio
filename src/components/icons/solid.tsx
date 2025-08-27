9403bad39f5cbe76cb99eb51a587f7e952460ad6
diegovfeder <63731464+diegovfeder@users.noreply.github.com>
2025-08-27 20:30:28 -0300

    fix: updates discontinued to archived props, updated nav, project component, and projects section

--- src/components/icons/solid.tsx
+++ src/components/icons/solid.tsx
@@ -1,4 +1,3 @@
-// FIXME: Center icons here. Better even to have them here instead of a lib.
 import { BsGithub, BsLinkedin, BsWindow } from 'solid-icons/bs'
 import { FiCode, FiGitPullRequest } from 'solid-icons/fi'
 import { IoCloseSharp } from 'solid-icons/io'
